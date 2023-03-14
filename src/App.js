709084878824fc91b3b241914c28a39d28bbbc03
kanpurin <kanpurin2@gmail.com>
2023-03-15 03:53:28 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import './App.css'
-import Form from './Form'
+import RandomArray from './RandomArray'
+import RandomBalancedBracketSequences from './RandomBalancedBracketSequences'
+import RandomLabeledTree from './RandomLabeledTree'
 
 function App() {
-
   return (
 		<div>
 			<h1 className='bg-primary text-white display-4'>Random Generator</h1>
-      <Form />
+      <RandomArray />
+      <RandomBalancedBracketSequences />
+      <RandomLabeledTree />
 		</div>
   );
 }
