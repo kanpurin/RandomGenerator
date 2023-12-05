a107bbec7b1dddb731c4cc36d35fcf972577b1e5
kanpurin <kanpurin2@gmail.com>
2023-12-05 22:52:48 +0900

    コピーボタンのデザインを変更

--- src/Result.js
+++ src/Result.js
@@ -1,43 +1,24 @@
 /* eslint no-undef: 0 */
-import React, { useState } from "react";
-// import ToolTip from "./ToolTip";
-import './Result.css'
+import React from "react";
+import CopyButton from "./CopyButton";
 
 function Result(props) {
-	const [show, setShow] = useState(false);
-  const text = "copy";
-
   const textTrim = (text, wordCount) => {
     if (text.length > wordCount) {
-      return text.substr(0,wordCount) + '...';
-    }
-    else {
+      return text.substr(0, wordCount) + '...';
+    } else {
       return text;
     }
-  }
+  };
 
-  const doCopy = (text) => {
-    navigator.clipboard.writeText(text);
-	}
-
-	return (
+  return (
     <>
-      <div className="input-group my-3">
-        <textarea className="form-control" value={textTrim(props.array.join(props.separate),1000)} rows={props.rows} readOnly/>
-        <button 
-          type="button" 
-          className="btn btn-outline-secondary" 
-          id="copybutton"
-          onClick={() => doCopy(props.array.join(props.separate))}
-          onMouseEnter={() => {setShow(true)}}
-          onMouseLeave={() => {setShow(false)}}
-        >
-          <i className="bi-clipboard-plus"></i>
-          { show && <div className='tooltip-top'>{text}</div> }
-        </button>
+      <div className="my-1 position-relative">
+        <textarea className="form-control" value={textTrim(props.array.join(props.separate), 1000)} rows={props.rows} readOnly />
+        <CopyButton text={props.array.join(props.separate)} />
       </div>
     </>
-	)
+  );
 }
 
-export default Result
+export default Result;
